120ad61087bc901c1771b233aa56d3212b788295
swan556 <swanand556@gmail.com>
2025-08-15 12:42:32 +0530

    done with backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
     
-    import Papa from 'papaparse/papaparse.min.js';
-	import { onMount } from 'svelte';
-	import { parse } from 'svelte/compiler';
-    let countries: Array<string> = [];
-    let selected = null;
+    let country_name: string = $state("");
+    let country_code: string = $state("");
+    let score: number = $state(0);
+    let error: string = $state("");
 
-    onMount(async() => {
-        const res = await fetch('/flags_dataset/dataset.csv');
-        const text = await res.text();
-        const parsed = Papa.parse(text, {header: true});
-        countries = parsed.data;
-    });
-
-    
     let dataset: string = "/flags_dataset/Flags/";
     let answer = $state("");
-
     
-    const randomFlag = () => {
-        const idx = Math.floor(Math.random()*countries.length);
-        selected = countries[idx];
-    }
+    
+
+    const getRandomFlag = async () => {
+
+        error = "";
+        if(answer === country_name) {
+            score++;
+        }
+        else score--;
+
+        answer = "";
+
+        const fetch_flag = async() => {
+            const res = await fetch('http://127.0.0.1:5000/random-flag');
+            let country = await res.json();
+
+            country_name = country.country_name;
+            country_code = country.country_code;
+        }
+
+        fetch_flag();
+        if(answer === "") {
+            error = "no empty spaces alowed";
+        }
+
+
+        }
+
 </script>
 
 <div class="app">
@@ -31,7 +45,8 @@
         </div>
 
         <div class="flag">
-            <img src="{dataset+"ad.png"}" alt="1" />
+            <img src={dataset + country_code + ".png"} alt="flag" />
+
         </div>
 
         <div class="options">
@@ -40,13 +55,17 @@
         </div>
 
         <div class="next">
-            <button class="nextButton">Next</button>
-
-            
+            <button class="nextButton" onclick={getRandomFlag}>Next</button>
         </div>
+        <div>score: {score}</div>
+        <div>your ans: {answer}</div>
+        <div>country name: {country_name}</div>
+
+        {#if (error !== "")}
+            <div style="color: red;">{error}</div>
+        {/if}
     </div>
 </div>
-
 
 
 <style>
