feb21d58d9fceef7e43aee0c61cb82eefba22207
swan556 <swanand556@gmail.com>
2025-08-15 18:34:45 +0530

    added some design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,118 +1,168 @@
 <script lang="ts">
-    
-    let country_name: string = $state("");
-    let country_code: string = $state("");
-    let score: number = $state(0);
-    let error: number = $state(0);
-    let startScreen: boolean = $state(true);
+	let country_name: string = $state('');
+	let country_code: string = $state('');
+	let score: number = $state(0);
+	let error: number = $state(0);
+	let startScreen: boolean = $state(true);
 
-    let dataset: string = "/flags_dataset/Flags/";
-    let answer = $state("");
-    
-    
-    const fetch_flag = async() => {
-        const res = await fetch('http://127.0.0.1:5000/random-flag');
-        let country = await res.json();
+	let dataset: string = '/flags_dataset/Flags/';
+	let answer = $state('');
 
-        country_name = country.country_name;
-        country_code = country.country_code;
-    }
+	const fetch_flag = async () => {
+		const res = await fetch('http://127.0.0.1:5000/random-flag');
+		let country = await res.json();
 
-    const toDashedString = ({str} : {str: string}): string => {
-        let modified_str: string = "";
-        for(let i = 0; i < str.length; i++) {
-            if(str[i] === " ") modified_str += "\x20";
-            else modified_str += "[]"
-        }
+		country_name = country.country_name;
+		country_code = country.country_code;
+	};
 
-        return modified_str;
-    }
+	const toDashedString = ({ str }: { str: string }): string => {
+		let modified_str: string = '';
+		for (let i = 0; i < str.length; i++) {
+			if (str[i] === ' ') modified_str += '\x20';
+			else modified_str += '[]';
+		}
 
+		return modified_str;
+	};
 
-    const checkAns = ({startMenu}: {startMenu: boolean}) => {
-        
-        if(!startMenu)
-        { 
-            if(answer !== "") {
-                if(answer.trim().toLowerCase() === country_name.toLowerCase()) {
-                    score += 1;
-                }
-                else score -= 1;
+	const checkAns = ({ startMenu }: { startMenu: boolean }) => {
+		if (!startMenu) {
+			if (answer !== '') {
+				if (answer.trim().toLowerCase() === country_name.toLowerCase()) {
+					score += 1;
+				} else score -= 1;
 
-                error = 0;
-                answer = "";
-                fetch_flag();
-            }
-
-            else {
-                error =1;
-            }}
-        else {
-            startScreen = false;
-            fetch_flag();
-        }
-    }
-
+				error = 0;
+				answer = '';
+				fetch_flag();
+			} else {
+				error = 1;
+			}
+		} else {
+			startScreen = false;
+			fetch_flag();
+		}
+	};
 </script>
 
 <div class="app">
+	{#if startScreen}
+		<div class="container">
+			<div class="start">
+				<button
+					class="startButton"
+					onclick={() => {
+						checkAns({ startMenu: true });
+					}}>START GAME</button
+				>
+			</div>
+		</div>
+	{:else}
+		<div class="container">
+			<div class="heading">Guess The Flag!</div>
 
-    {#if (startScreen)}
-        <div class="container">
-            <div class="start">
-                <button class="startButton" onclick={() => {
-                    checkAns({startMenu: true});
-                }}>START GAME</button>
-            </div>
-        </div>
-    {:else}
-        <div class="container">
-            <div class="heading">
-                Guess The Flag!
-            </div>
+			<div class="flag">
+				<img
+					src={dataset + country_code + '.png'}
+					alt="flag"
+					style="width: 300px; height: 200px;"
+				/>
+			</div>
 
-            <div class="flag">
-                <img src={dataset + country_code + ".png"} alt="flag" style="width: 300px; height: 200px;" />
-            </div>
+			<div class="options">
+				<label for="input">Answer</label>
+				<input
+					type="text"
+					bind:value={answer}
+					id="input"
+					onkeydown={(e) => {
+						if (e.key === 'Enter') {
+							checkAns({ startMenu: false });
+						}
+					}}
+				/>
+			</div>
 
-            <div class="options">
-                <label for="input">Answer</label>
-                <input type="text" bind:value={answer} id="input" onkeydown={(e) => {
-                    if(e.key === "Enter") {
-                        checkAns({startMenu: false});
-                    }
-                }}/>
-            </div>
+			<div class="next">
+				<button
+					class="nextButton"
+					onclick={() => {
+						checkAns({ startMenu: false });
+					}}>Next</button
+				>
+			</div>
+			<div>score: {score}</div>
+			<div>your ans: {answer}</div>
+			<div>Hint: {toDashedString({ str: country_name })}</div>
 
-            <div class="next">
-                <button class="nextButton" onclick={() => {checkAns({startMenu: false})}}>Next</button>
-            </div>
-            <div>score: {score}</div>
-            <div>your ans: {answer}</div>
-            <div>Hint: {toDashedString({str: country_name})}</div>
-
-            {#if (error)}
-                <div style="color: red;">You cannot submit blank answer</div>
-            {/if}
-        </div>
-
-    {/if}
+			{#if error}
+				<div style="color: red;">You cannot submit blank answer</div>
+			{/if}
+		</div>
+	{/if}
 </div>
 
-
 <style>
-    .app {
-        display: flex;
-        align-items: center;
-        align-self: center;
-        justify-content: center;
-        height: 100vh;
+	.app {
+		display: flex;
+		align-items: center;
+		align-self: center;
+		justify-content: center;
+		height: 100vh;
+        background-color: rgb(43, 43, 43);
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+        height: 100%;
+	}
+
+    .startButton {
+        position: absolute;
+        top:50%;
+        bottom: 50%;
+
+        transform: translate(-50%, -50%);
+
+        border-radius: 8px;
+        width: 500px;
+        height: 200px;
+        outline: none;
+        border: 7px solid;
+        border-color: rgb(201, 187, 0);
+        border-radius: 50px;
+        background-color: black;
+        color: white;
+        font-size: 40px;
+        font-style:normal;
+        font-family:Georgia, 'Times New Roman', Times, serif;
+
+        transition: border-color 0.5s ease, box-shadow 0.5s ease;
     }
 
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .startButton:hover {
+        border-color: transparent;
+        box-shadow: 0 0 20px 5px limegreen;
+        font-size: 50px;
+        color: rgb(0, 250, 104);
+    }
+
+    .heading {
+        font-family: "Comic Sans MS", "Comic Sans", cursive;
+        color: white;
+        font-size: 100px;
+        margin-top: 90px;
+        font-weight: 1000;
+    }
+
+    .flag {
+        border: 10px solid;
+        border-color: white;
+        border-radius: 10px;
+        margin-top: 100px;
     }
 
 
