00e7f18794c4e927376b005fc2f528989e1fa882
swan556 <swanand556@gmail.com>
2025-08-16 14:46:37 +0530

    changes to package.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
   }
 };
 
